3881d0ad6ab94a4de209ca3cbec9d0a6153fc6f6
Marcin Chylek <marcin@chylek.pl>
2015-11-03 12:04:58 +0100

    Add composer bundle

---
+++ MessenteBundle.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of PingerBundle.
+ *
+ * (c) Marcin Chyłek <marcin@chylek.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ampluso\MessenteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MessenteBundle extends Bundle
+{
+
+}
